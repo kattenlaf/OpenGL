f068613022fea5cd248224269337ddb0ca174265
kattenlaf <ruel.gordon123@gmail.com>
2024-12-20 23:55:19 -0800

    used index buffers to draw two triangles (square) on screen

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -40,10 +40,17 @@ int main(void) {
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6]{
+    float positions[]{
         -0.5f, -0.5f,
-         0.0f,  0.5f,
          0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
+    };
+
+    // Index buffer so we can render a square
+    unsigned int indices[] = {
+        0, 1, 2, // First Triangle
+        2, 3, 0 // Second Triangle
     };
 
     // docs.gl documentation to learn and properly understand, the cherno episode 5
@@ -51,11 +58,17 @@ int main(void) {
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW); //https://docs.gl/gl4/glVertexAttribPointer
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW); //https://docs.gl/gl4/glVertexAttribPointer
 
     // Try to remember what the index parameter here does, possibly 0 indicates in the positions array where the vertices start? I don't remember
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    // Set index buffer on gpu, specifies the specific vertices to be drawn instead of positions storing copies of the same vertex
+    unsigned int ibo; // index buffer object
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     std::string filepath = "res/shaders/Basic.shader";
     ShaderProgramSource gfx_source = ParseShader(filepath);
@@ -69,7 +82,8 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Draw the Triangle here */
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 6); // No longer need glDrawArrays when using index buffers, glDrawElements instead
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
