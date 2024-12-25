285c98ff3292e3dc7bb0af45fdfc89354b1f4219
kattenlaf <ruel.gordon123@gmail.com>
2024-12-24 16:57:15 -0800

    Handle opengl errors with new macro func

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -5,6 +5,11 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
@@ -15,6 +20,8 @@ void drawTriangleLegacy(float* arr);
 static unsigned int CompileShader(unsigned int type, const std::string& source);
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 static ShaderProgramSource ParseShader(const std::string& filepath);
+static void GLClearError();
+static bool GLLogCall(const char* function, const char* file, int line);
 
 int main(void) {
     GLFWwindow* window;
@@ -56,25 +63,25 @@ int main(void) {
     // docs.gl documentation to learn and properly understand, the cherno episode 5
     // Bind buffers, specify array buffer for vertices
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW); //https://docs.gl/gl4/glVertexAttribPointer
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW)); //https://docs.gl/gl4/glVertexAttribPointer
 
     // Try to remember what the index parameter here does, possibly 0 indicates in the positions array where the vertices start? I don't remember
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
     // Set index buffer on gpu, specifies the specific vertices to be drawn instead of positions storing copies of the same vertex
     unsigned int ibo; // index buffer object
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
     std::string filepath = "res/shaders/Basic.shader";
     ShaderProgramSource gfx_source = ParseShader(filepath);
 
     unsigned int shader = CreateShader(gfx_source.VertexSource, gfx_source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -82,8 +89,7 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Draw the Triangle here */
-        // glDrawArrays(GL_TRIANGLES, 0, 6); // No longer need glDrawArrays when using index buffers, glDrawElements instead
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -92,7 +98,7 @@ int main(void) {
         glfwPollEvents();
     }
 
-    glDeleteProgram(shader);
+    GLCall(glDeleteProgram(shader));
 
     glfwTerminate();
     return 0;
@@ -111,22 +117,22 @@ void drawTriangleLegacy(float* arr) {
 }
 
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
-    unsigned int id = glCreateShader(type);
+    GLCall(unsigned int id = glCreateShader(type));
     const char* src = source.c_str(); // OpenGL expects source string in this format
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
+    GLCall(glShaderSource(id, 1, &src, nullptr));
+    GLCall(glCompileShader(id));
 
     int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    GLCall(glGetShaderiv(id, GL_COMPILE_STATUS, &result));
     if (result == GL_FALSE) {
         int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        GLCall(glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length));
         // char message[length]; doesn't work allocating on the stack because c++ expects constant value to initialize array like char message[10]
         char* message = (char*)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
+        GLCall(glGetShaderInfoLog(id, length, &length, message));
         std::cout << "Failed to compile shader " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
         std::cout << message << std::endl;
-        glDeleteShader(id);
+        GLCall(glDeleteShader(id));
         return 0;
     }
 
@@ -138,13 +144,13 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
+    GLCall(glAttachShader(program, vs));
+    GLCall(glAttachShader(program, fs));
+    GLCall(glLinkProgram(program));
+    GLCall(glValidateProgram(program));
 
-    glDeleteShader(vs);
-    glDeleteShader(fs);
+    GLCall(glDeleteShader(vs));
+    GLCall(glDeleteShader(fs));
 
     return program;
 }
@@ -181,4 +187,16 @@ static ShaderProgramSource ParseShader(const std::string& filepath) {
     source.FragmentSource = ss[int(ShaderType::FRAGMENT)].str();
 
     return source; // Could also do { ss[0].str(), ss[1].str() } but keeping the above since more readable over time imo
+}
+
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "OpenGL Error: (" << error << ") " << function << " " << file << ":" << line <<  std::endl;
+        return false;
+    }
+    return true;
 }
