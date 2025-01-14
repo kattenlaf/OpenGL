2815d9e928db66bb9227d4464b9a8b08aefca9c3
kattenlaf <ruelgordon123@gmail.com>
2025-01-13 19:55:31 -0800

    introduce texture test method

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -13,6 +13,7 @@
 #include "Shader.h"
 #include "Texture.h"
 
+// Math libraries
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
@@ -20,6 +21,7 @@
 #include "imgui/imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void) {
     GLFWwindow* window;
@@ -67,6 +69,7 @@ int main(void) {
         currentTest = testMenu;
 
         testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+        testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
         while (!glfwWindowShouldClose(window)) {
             /* Render here */
